4c006792678d73c6ac30c2d13a7a52d6ca84965d
Eugene Romankov <iromankov2010@gmail.com>
2018-02-27 14:59:13 +0300

    Add modules(Long Arithmetics): 1. Long Addition 2. Long Subtraction 3. Long Multiplication 4. Long Division 5. Sqrt function Add conection in index.js with modules(Long Arithmetics).

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+const longAddition = require('./longAddition');
+const longSubtraction = require('./longSubtraction');
+const longMultiplication = require('./longMultiplication');
+const longDivision = require('./longDivision');
+const sqrtFunction = require('./sqrtFunction');
+
 module.exports = function solveEquation(equation) {
     return -1;
 }
