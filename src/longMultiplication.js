49e50d012d7aec0f8cd96db957cb6e472f17899e
Eugene Romankov <iromankov2010@gmail.com>
2018-02-27 20:49:43 +0300

    Fix longMultiplication() function: delete resArrLen variable, modify some if conditions

--- src/longMultiplication.js
+++ src/longMultiplication.js
@@ -4,28 +4,18 @@ module.exports = function longMultiplication(strNum1, strNum2) {
     let secondNumArr = strNum2.toString().split("").reverse();
     let secondNumArrLen = secondNumArr.length;
     let resArr = [];
-    let resArrLen = resArr.length;
 
     for (let i = 0; i < firstNumArrLen; i++) {
         for (let j = 0; j < secondNumArrLen; j++) {
             let currentNdx = i + j;
 
-            if (!(currentNdx >= resArrLen)) {
-                resArr[currentNdx] = firstNumArr[i] * secondNumArr[j] + resArr[currentNdx];
-            } else {
-                resArr[currentNdx] = firstNumArr[i] * secondNumArr[j];
-            }
+            resArr[currentNdx] = firstNumArr[i] * secondNumArr[j] + (currentNdx >= resArr.length ? 0 : resArr[currentNdx]);
 
             if (resArr[currentNdx] > 9) {
-                if (!((currentNdx + 1) >= resArrLen)) {
-                    resArr[currentNdx + 1] = Math.floor(resArr[currentNdx] / 10) + resArr[currentNdx + 1];
-                } else {
-                    resArr[currentNdx + 1] = Math.floor(resArr[currentNdx] / 10);
-                }
-
-                resArr[currentNdx] -= resArr[currentNdx] % 10;
+                resArr[currentNdx + 1] = Math.floor(resArr[currentNdx] / 10) + (currentNdx + 1 >= resArr.length ? 0 : resArr[currentNdx + 1]);
+                resArr[currentNdx] -= Math.floor(resArr[currentNdx] / 10) * 10;
             }
         }
     }
-    return resArr.reverse().join();
+    return resArr.reverse().join("");
 }
