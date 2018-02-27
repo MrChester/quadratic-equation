593495366092e4e9a3b49a73a3911a9d0c640235
Eugene Romankov <iromankov2010@gmail.com>
2018-02-27 18:44:43 +0300

    Add longMultiplication() implementation

--- src/longMultiplication.js
+++ src/longMultiplication.js
@@ -1,3 +1,30 @@
-module.exports = function longMultiplication() {
+module.exports = function longMultiplication(strNum1, strNum2) {
+    let firstNumArr = strNum1.split("").reverse();
+    let firstNumArrLen = firstNumArr.length;
+    let secondNumArr = strNum2.toString().split("").reverse();
+    let secondNumArrLen = secondNumArr.length;
+    let resArr = [];
+    let resArrLen = resArr.length;
 
+    for (let i = 0; i < firstNumArrLen; i++) {
+        for (let j = 0; j < secondNumArrLen; j++) {
+            let currentNdx = i + j;
+
+            if (!(currentNdx >= resArrLen)) {
+                resArr[currentNdx] = firstNumArr[i] * secondNumArr[j] + resArr[currentNdx];
+            } else {
+                resArr[currentNdx] = firstNumArr[i] * secondNumArr[j];
+            }
+
+            if (resArr[currentNdx] > 9) {
+                if (!((currentNdx + 1) >= resArrLen)) {
+                    resArr[currentNdx + 1] = Math.floor(resArr[currentNdx] / 10) + resArr[currentNdx + 1];
+                } else {
+
+                }
+                resArr[currentNdx + 1] = Math.floor(resArr[currentNdx] / 10);
+                resArr[currentNdx] -= resArr[currentNdx] % 10;
+            }
+        }
+    }
 }
