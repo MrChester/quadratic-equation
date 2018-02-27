21d2e412e4fbbd8dff529ae05d0dfc9a9a50c11d
Eugene Romankov <iromankov2010@gmail.com>
2018-02-27 15:45:01 +0300

    Add some changes in solveEquation()

--- src/index.js
+++ src/index.js
@@ -5,5 +5,21 @@ const longDivision = require('./longDivision');
 const sqrtFunction = require('./sqrtFunction');
 
 module.exports = function solveEquation(equation) {
-    return -1;
+    let resArr = [];
+    let a, b, c, x1, x2, x, discriminant;
+
+    discriminant = Math.sqrt(b * b - 4 * a * c);
+
+    if (d > 0) {
+        x1 = (-b + discriminant) / 2 * a;
+        x2 = (-b - discriminant) / 2 * a;
+        resArr.push(x1);
+        resArr.push(x2);
+    }
+    if (d === 0) {
+        x = -b / 2 * a;
+    }
+
+
+    return resArr.sort();
 }
