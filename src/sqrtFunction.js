378c9637c70e5fea28239f8e1e3cf59d70dd2ace
Eugene Romankov <iromankov2010@gmail.com>
2018-02-27 15:52:33 +0300

    Add sqrtFunc() implementation.

--- src/sqrtFunction.js
+++ src/sqrtFunction.js
@@ -1,3 +1,11 @@
-module.exports = function sqrtFunc() {
+module.exports = function sqrtFunc(number) {
+    var t;
+    var squareRoot = number / 2;
 
+    do {
+        t = squareRoot;
+        squareRoot = (t + (number / t)) / 2;
+
+    } while ((t - squareRoot) !== 0);
+    return squareRoot;
 }
