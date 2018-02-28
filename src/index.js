f9392976a7e1e2bba41ae60b64493e4fc08d628a
Eugene Romankov <iromankov2010@gmail.com>
2018-02-28 18:08:45 +0300

    Fix index.js: fixed some mistakes in code

--- src/index.js
+++ src/index.js
@@ -5,29 +5,33 @@ const longDivision = require('./longDivision');
 const sqrtFunc = require('./sqrtFunction');
 
 module.exports = function solveEquation(equation) {
-        var resArr = [],
-            a, b, c, x1, x2, x, discriminant,
-            regexp = /-\d+|\d+/gi,
-            regSpaces = /\s+|\b\^\w/gi,
-            tempArr = equation.replace(regSpaces, ''),
-            numArr = tempArr.math(regexp);
+    var resArr = [],
+        a, b, c, x1, x2, x, discriminant,
+        regexp = /-\d+|\d+/gi,
+        regSpaces = /\s+|\b\^\w/gi,
+        tempArr = equation.replace(regSpaces, ''),
+        numArr = tempArr.match(regexp);
 
-        a = numArr[0];
-        b = numArr[1];
-        c = numArr[2];
+    a = numArr[0];
+    b = numArr[1];
+    c = numArr[2];
 
-        discriminant = sqrtFunc(longMultiplication(b, b) - longMultiplication(4, longMultiplication(a, c));
+    console.log(a);
+    console.log(b);
+    console.log(c);
 
-            if (d > 0) {
-                x1 = (-b + discriminant) / 2 * a;
-                x2 = (-b - discriminant) / 2 * a;
-                resArr.push(x1);
-                resArr.push(x2);
-            }
-            if (d === 0) {
-                x = -b / 2 * a;
-            }
+    discriminant = sqrtFunc(longMultiplication(b, b) - longMultiplication(4, longMultiplication(a, c)));
+
+    if (d > 0) {
+        x1 = (-b + discriminant) / 2 * a;
+        x2 = (-b - discriminant) / 2 * a;
+        resArr.push(x1);
+        resArr.push(x2);
+    }
+    if (d === 0) {
+        x = -b / 2 * a;
+    }
 
 
-            return resArr.sort();
-        }
+    return resArr.sort();
+}
