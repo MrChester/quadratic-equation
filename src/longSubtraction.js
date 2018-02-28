3ffdbfb949b1b2e3918df5f8ea1c8eb2a11ba54a
Eugene Romankov <iromankov2010@gmail.com>
2018-02-28 23:33:50 +0300

    Create numComparison.js Add implementation for numComaprison() function: this function compare array length and digits of numbers in two different arrays. Fix longSubtraction.js: add base variables;

--- src/longSubtraction.js
+++ src/longSubtraction.js
@@ -1,3 +1,13 @@
-module.exports = function longSubtraction() {
+module.exports = function longSubtraction(strNum1, strNum2) {
+    let firstNumArr = strNum1.replace(/-/g, '').split("").reverse(),
+        secondNumArr = strNum2.replace(/-/g, '').split("").reverse(),
+        firstNumArrLen = firstNumArr.length,
+        secondNumArrLen = secondNumArr.length,
+        resArr = [],
+        sign = -1;
+
+    for (let i = 0; i < len; i++) {
+
+    }
 
 }
---
+++ src/numComparison.js
@@ -0,0 +1,29 @@
+module.exports = function numComaprison(strNum1, strNum2) {
+    let firstNumArr = strNum1.replace(/-/g, '').split("").reverse(),
+        secondNumArr = strNum2.replace(/-/g, '').split("").reverse(),
+        firstNumArrLen = firstNumArr.length,
+        secondNumArrLen = secondNumArr.length,
+        flag = 3,
+        len = firstNumArr.length;
+
+    if (firstNumArrLen > secondNumArrLen) {
+        flag = 1;
+        len = firstNumArrLen;
+    } else if (secondNumArrLen > firstNumArrLen) {
+        flag = 2;
+        len = secondNumArrLen;
+    } else {
+        for (let i = 0; i < len;) {
+            if (firstNumArr[i] > secondNumArr[i]) {
+                flag = 1;
+                break;
+            }
+            if (secondNumArr[i] > firstNumArr[i]) {
+                flag = 2;
+                break;
+            }
+        }
+    }
+
+    return flag;
+}
