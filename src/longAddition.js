5e3a0d3ab6bef96cf47070b695a840817556db70
Eugene Romankov <iromankov2010@gmail.com>
2018-02-28 18:29:36 +0300

    Fix longAddition.js: implementation of longAddition() completed.

--- src/longAddition.js
+++ src/longAddition.js
@@ -1,5 +1,19 @@
 module.exports = function longAddition(strNum1, strNum2) {
-    let firstNumArr = strNum1.replace(/-/g, '').split("").reverse();
-    let secondNumArr = strNum2.replace(/-/g, '').split("").reverse();
+    let firstNumArr = strNum1.replace(/-/g, '').split("").reverse(),
+        secondNumArr = strNum2.replace(/-/g, '').split("").reverse(),
+        len_a = firstNumArr.length,
+        len_b = secondNumArr.length,
+        len = Math.max(len_a, len_b),
+        accuracy = 0,
+        resArr = [];
 
+    for (let i = 0; i < len; i++) {
+        let temp = Number(firstNumArr[i] || 0) + Number(secondNumArr[i] || 0) + accuracy;
+        accuracy = Math.floor(temp / 10);
+        resArr.push(temp % 10);
+    }
+    if (accuracy !== 0) {
+        resArr.push(accuracy);
+    }
+    return resArr.reverse().join('');
 }
